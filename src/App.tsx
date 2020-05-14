3e186ca1f864ebe0a95b150833132f5be5b9925e
danielbartsch <daniel.bartsch@adverity.com>
2020-05-14 22:04:43 +0200

    added colors and proper modal to edit events

--- src/App.tsx
+++ src/App.tsx
@@ -99,17 +99,130 @@ type LocalStorageEvents = {
   [key: string]: Event
 }
 
+type ModalState = {
+  content: React.ReactNode
+  show: boolean
+  position: { x: number; y: number }
+}
+
+const useModal = (
+  initialState: ModalState = {
+    content: null,
+    show: false,
+    position: { x: window.innerWidth / 2, y: window.innerHeight / 2 },
+  }
+) => {
+  const [modal, setModal] = React.useState<ModalState>(initialState)
+
+  return {
+    modal,
+    open: (content: ModalState['content'], position: ModalState['position']) =>
+      setModal({ content, position, show: true }),
+    close: () => setModal(prev => ({ ...prev, show: false })),
+  }
+}
+
+const Modal = ({ show, content, position, close }: ModalState & { close: () => void }) => {
+  return show ? (
+    <>
+      <div className="backdrop" onClick={close} />
+      <div className="modal" style={{ top: position.y, left: position.x }}>
+        {content}
+      </div>
+    </>
+  ) : null
+}
+
+const eventColors = [
+  '#553333',
+  '#594435',
+  '#3e3b26',
+  '#29443f',
+  '#273944',
+  '#332b4d',
+  '#412b4d',
+  '#40233e',
+  '#462324',
+]
+
+const EventForm = ({
+  initialEvent,
+  onDescriptionApply,
+  onColorChange,
+  onDelete,
+}: {
+  initialEvent: Event
+  onDescriptionApply: (description: Event['description']) => void
+  onColorChange: (color: Event['color']) => void
+  onDelete: () => void
+}) => {
+  const [description, setDescription] = React.useState(initialEvent.description)
+  const [color, setColor] = React.useState(initialEvent.color)
+
+  return (
+    <>
+      <div className="flexEven fullWidth">
+        <input
+          autoFocus
+          type="text"
+          className="fullWidth"
+          placeholder="Eventbeschreibung"
+          value={description}
+          onChange={event => setDescription(event.target.value)}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              onDescriptionApply(description)
+            }
+          }}
+        />
+        <svg
+          onClick={onDelete}
+          style={{ position: 'relative' }}
+          width="16"
+          height="16"
+          viewBox="0 0 12 12"
+        >
+          <path d="M4,11 l4,0 l2,-6 l-8,0z M2,4 l8,0 l-1,-2 l-6,0z" fill="#f55" />
+        </svg>
+      </div>
+      <div className="flexEven fullWidth" style={{ marginTop: 8 }}>
+        {eventColors.map(currentColor => (
+          <div
+            key={currentColor}
+            onClick={() => {
+              setColor(currentColor)
+              onColorChange(currentColor)
+            }}
+            className="colorSwatch"
+            style={{
+              borderColor: currentColor === color ? '#999' : '#484848',
+              backgroundColor: currentColor,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   const [startDay, setStartDay] = React.useState(getDateWithoutTime(new Date()))
   const [shownDays, setDays] = React.useState(30)
   const [events, setEvents] = React.useState<LocalStorageEvents>(() =>
     JSON.parse(localStorage.getItem('events') ?? '{}')
   )
+  const { modal, open, close } = useModal()
 
   const handleSetEvents = (newEvents: LocalStorageEvents) => {
     setEvents(newEvents)
     localStorage.setItem('events', JSON.stringify(newEvents))
   }
+
+  useEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      close()
+    }
+  })
 
   useEventListener('wheel', (event: WheelEvent) => {
     if (event.deltaY !== 0) {
@@ -146,107 +259,119 @@ const App = () => {
   })
 
   return (
-    <table style={{ width: '100%' }}>
-      <thead>
-        <tr>
-          <th style={{ width: 150 }}>Datum</th>
-          <th style={{ width: 150 }}>Feiertag</th>
-          <th>Geburtstag</th>
-          <th style={{ width: 20 }}>Werktag</th>
-          <th style={{ width: 20 }}>Wochenende</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Array.from({ length: shownDays }).map((_, index) => {
-          const date = addDays(startDay, index)
+    <>
+      <table className="fullWidth">
+        <thead>
+          <tr>
+            <th style={{ width: 150 }}>Datum</th>
+            <th style={{ width: 150 }}>Feiertag</th>
+            <th>Geburtstag</th>
+            <th style={{ width: 20 }}>Werktag</th>
+            <th style={{ width: 20 }}>Wochenende</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: shownDays }).map((_, index) => {
+            const date = addDays(startDay, index)
 
-          const todaysHolidays = holidays
-            .filter(([, isHoliday]) => isHoliday(date))
-            .map(([name]) => name)
+            const todaysHolidays = holidays
+              .filter(([, isHoliday]) => isHoliday(date))
+              .map(([name]) => name)
 
-          const todaysBirthdays = birthdays.filter(
-            ([, birthday]) =>
-              birthday.getMonth() === date.getMonth() &&
-              birthday.getDate() === date.getDate() &&
-              date.getFullYear() >= birthday.getFullYear()
-          )
+            const todaysBirthdays = birthdays.filter(
+              ([, birthday]) =>
+                birthday.getMonth() === date.getMonth() &&
+                birthday.getDate() === date.getDate() &&
+                date.getFullYear() >= birthday.getFullYear()
+            )
 
-          const isWorkday = workDays.includes(date.getDay())
-          const isWeekend = weekendDays.includes(date.getDay())
+            const isWorkday = workDays.includes(date.getDay())
+            const isWeekend = weekendDays.includes(date.getDay())
 
-          const eventColors = [
-            '#553333',
-            '#594435',
-            '#3e3b26',
-            '#29443f',
-            '#273944',
-            '#332b4d',
-            '#412b4d',
-            '#40233e',
-            '#462324',
-          ]
+            const eventKey = date.toString()
 
-          const eventKey = date.toString()
+            const event = events[eventKey] ?? {}
+            const backgroundColor = isEqual(date, new Date())
+              ? '#343'
+              : event
+              ? event.color
+              : undefined
 
-          const { description, color } = events[eventKey] ?? {}
-          const backgroundColor = isEqual(date, new Date())
-            ? '#343'
-            : events[eventKey]
-            ? color
-            : undefined
-
-          return (
-            <tr
-              key={`${date}`}
-              style={backgroundColor ? { backgroundColor } : undefined}
-              onClick={() => {
-                const appointmentDescription = prompt(
-                  `Termin für ${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
-                  description
-                )
-                if (appointmentDescription) {
-                  const updatedEvents = {
-                    ...events,
-                    [eventKey]: {
-                      description: appointmentDescription,
-                      color: eventColors[parseInt('' + Math.random() * eventColors.length)],
-                    },
-                  }
-                  handleSetEvents(updatedEvents)
-                } else if (appointmentDescription === '') {
-                  const { [eventKey]: ignored, ...currentDateOmittedEvents } = events
-                  handleSetEvents(currentDateOmittedEvents)
-                }
-              }}
-              title={description}
-            >
-              <td className="date">
-                <DayStyle
-                  date={date}
-                  todaysHolidays={todaysHolidays}
-                  todaysBirthdays={todaysBirthdays}
-                >
-                  <Day value={date} />
-                </DayStyle>
-              </td>
-              <td>{todaysHolidays.length > 0 ? todaysHolidays.join(', ') : undefined}</td>
-              <td>
-                {todaysBirthdays.length > 0
-                  ? todaysBirthdays
-                      .map(
-                        ([name, birthday]) =>
-                          `${name} (${date.getFullYear() - birthday.getFullYear()})`
-                      )
-                      .join(', ')
-                  : undefined}
-              </td>
-              <td>{isWorkday && todaysHolidays.length === 0 ? 'o' : undefined}</td>
-              <td>{isWeekend ? 'o' : undefined}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                key={`${date}`}
+                style={backgroundColor ? { backgroundColor } : undefined}
+                onClick={({ clientX, clientY }) => {
+                  const modalDimensions = { x: 180, y: 50 }
+                  open(
+                    <EventForm
+                      key={eventKey}
+                      initialEvent={event}
+                      onDescriptionApply={newDescription => {
+                        if (newDescription) {
+                          const updatedEvents = {
+                            ...events,
+                            [eventKey]: {
+                              description: newDescription,
+                              color:
+                                event.color ??
+                                eventColors[parseInt('' + Math.random() * eventColors.length)],
+                            },
+                          }
+                          handleSetEvents(updatedEvents)
+                        }
+                        close()
+                      }}
+                      onColorChange={newColor => {
+                        const updatedEvents = {
+                          ...events,
+                          [eventKey]: {
+                            description: event.description,
+                            color: newColor,
+                          },
+                        }
+                        handleSetEvents(updatedEvents)
+                      }}
+                      onDelete={() => {
+                        const { [eventKey]: ignored, ...currentDateOmittedEvents } = events
+                        handleSetEvents(currentDateOmittedEvents)
+                        close()
+                      }}
+                    />,
+                    { x: clientX - modalDimensions.x / 2, y: clientY - modalDimensions.y / 2 }
+                  )
+                }}
+                title={event.description}
+              >
+                <td className="date">
+                  <DayStyle
+                    date={date}
+                    todaysHolidays={todaysHolidays}
+                    todaysBirthdays={todaysBirthdays}
+                  >
+                    <Day value={date} />
+                  </DayStyle>
+                </td>
+                <td>{todaysHolidays.length > 0 ? todaysHolidays.join(', ') : undefined}</td>
+                <td>
+                  {todaysBirthdays.length > 0
+                    ? todaysBirthdays
+                        .map(
+                          ([name, birthday]) =>
+                            `${name} (${date.getFullYear() - birthday.getFullYear()})`
+                        )
+                        .join(', ')
+                    : undefined}
+                </td>
+                <td>{isWorkday && todaysHolidays.length === 0 ? 'o' : undefined}</td>
+                <td>{isWeekend ? 'o' : undefined}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <Modal {...modal} close={close} />
+    </>
   )
 }
 
